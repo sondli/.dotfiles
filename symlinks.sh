6ad90432b3922de35339bbc804492558bf1602fb
sondli <sondre.lillelien@gmail.com>
2023-09-27 14:18:37 +0200

    added higher dpi, bashrc and profile, and systray to polybar

--- symlinks.sh
+++ symlinks.sh
@@ -4,6 +4,8 @@ DOTFILES="$HOME/.dotfiles"
 
 echo "Creating symlinks..."
 
+ln -s $DOTFILES/bash/.bashrc ~/
+ln -s $DOTFILES/bash/.bash_profile ~/
 ln -s $DOTFILES/nvim ~/.config
 ln -s $DOTFILES/tmux ~/.config
 ln -s $DOTFILES/polybar ~/.config
