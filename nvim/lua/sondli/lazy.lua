72ab1ec9c206b21ba8fa4d546a552fdf85955bfd
sondli <sondre.lillelien@gmail.com>
2023-09-29 23:27:10 +0200

    added the rest of ThePrimeagen nvim config

--- nvim/lua/sondli/lazy.lua
+++ nvim/lua/sondli/lazy.lua
@@ -12,19 +12,20 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{ 'nvim-telescope/telescope.nvim', tag = '0.1.3', dependencies = { 'nvim-lua/plenary.nvim' } },
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-    { "dylanaraps/wal.vim" },
-	{ 'ThePrimeagen/harpoon' },
-	{ "tpope/vim-fugitive" },
-	-- LSP ZERO
-	{'williamboman/mason.nvim'},
-	{'williamboman/mason-lspconfig.nvim'},
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'L3MON4D3/LuaSnip'},
+  { "nvim-telescope/telescope.nvim", tag = "0.1.3", dependencies = { "nvim-lua/plenary.nvim" } },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "dylanaraps/wal.vim" },
+  { "ThePrimeagen/harpoon" },
+  { "tpope/vim-fugitive" },
+  { "mbbill/undotree" },
+  -- LSP ZERO
+  {"williamboman/mason.nvim"},
+  {"williamboman/mason-lspconfig.nvim"},
+  {"VonHeikemen/lsp-zero.nvim", branch = "v3.x"},
+  {"neovim/nvim-lspconfig"},
+  {"hrsh7th/cmp-nvim-lsp"},
+  {"hrsh7th/nvim-cmp"},
+  {"L3MON4D3/LuaSnip"},
 
 
 })
--- nvim/lua/sondli/set.lua
+++ nvim/lua/sondli/set.lua
@@ -1,9 +1,9 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -27,3 +27,4 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "00"
 
 vim.g.mapleader = " "
+
