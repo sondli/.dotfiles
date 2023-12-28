0014eaf1a35caed282270262232f55cb8a6e4f4c
sondli <sondre.lillelien@gmail.com>
2023-12-28 17:19:06 +0100

    updates

--- cp-ideavim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-#setx WSLENV USERPROFILE/up
-#
-#This should make the windows user profile available as as ENV in wsl
-
-echo "Copying .ideavimrc..."
-
-cp $DOTFILES/ideavim/.ideavimrc $USERPROFILE/.ideavimrc
--- todo.sh
+++
@@ -1,2 +0,0 @@
-#( lsb_release -ds || cat /etc/*release || uname -om ) 2>/dev/null | head -n1
-#[ "${PATH#*$HOME/bin:}" == "$PATH" ] && export PATH="$HOME/bin:$PATH"
