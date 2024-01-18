715e0c53a6660a5e9b106f2d3c919982188753bd
sondli <sondre.lillelien@gmail.com>
2024-01-18 14:59:44 +0100

    added screen snipping

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup {
     options = {
         icons_enabled = true,
-        theme = 'catpuccin',
+        theme = 'auto',
         component_separators = '|',
         section_separators = { left = '', right = '' },
         disabled_filetypes = {
